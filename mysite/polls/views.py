6d665e14eb57c434a81a40c9e801a86340499150
bruno <bnsantos17@gmail.com>
2013-12-10 10:48:01 -0200

    Details view page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from polls.models import Poll
-from django.template import RequestContext, loader
 
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % poll_id)
